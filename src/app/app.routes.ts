aa4cc5c5c0086f45fc5dfb756a934cd2d2bfd2e4
JohnC <ionutcosarca1@gmail.com>
2024-05-27 17:10:24 +0300

    Created Query Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { QuestionComponent } from "./component/question/question.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: QuestionComponent }
+];
