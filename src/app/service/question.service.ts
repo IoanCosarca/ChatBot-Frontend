be70ad82f4c7d826f2236bc119584205a0702163
JohnC <ionutcosarca1@gmail.com>
2024-05-28 18:59:16 +0300

    Implemented sending question and receiving answer

---
+++ src/app/service/question.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+
+import { AnswerModel } from "../model/answer.model";
+import { QuestionModel } from "../model/question.model";
+
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+  constructor(private httpClient: HttpClient) { }
+
+  public askQuestion(question: QuestionModel): Observable<AnswerModel> {
+    return this.httpClient.post<AnswerModel>(`http://127.0.0.1:8080/ai`, question);
+  }
+}
